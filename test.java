edae5a183300859a1abc78366784e07f8941d0ee
coo19amer <asylbekzhailaubaev@gmail.com>
2021-02-08 22:12:12 +0600

    test.java now has for cycle that prints nums from 1 to 10

--- test.java
+++ test.java
@@ -3,5 +3,8 @@ public class test{
         System.out.println("this is giiit");
         System.out.println("this has changed now");
         System.out.println("this is githuub");
+        for(int i = 1;i <= 10;i++){
+            System.out.print(i + "\n");
+        }
     }
 }
