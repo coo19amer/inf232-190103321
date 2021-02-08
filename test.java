57ae445651121de2a18599e845695572e7288a9a
coo19amer <asylbekzhailaubaev@gmail.com>
2021-02-08 22:06:14 +0600

    change in test.java

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 public class test{
     public static void main(String args[]) {
         System.out.println("this is giiit");
+        System.out.println("this has changed now");
+        System.out.println("this is githuub");
     }
 }
